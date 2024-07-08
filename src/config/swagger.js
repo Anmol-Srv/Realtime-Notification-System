3c05189a0f87104f815712a815848ab69b6893bb
Anmol Srivastava <anmolsrivastava3000@gmail.com>
2024-07-09 04:16:25 +0530

    update Rabbit MQ URL,domain link update in swagger.js, remove vercel.json

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -12,7 +12,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://realtime-notification-system.onrender.com/',
       },
     ],
     components: {
